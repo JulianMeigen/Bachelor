1fac90e8c15242b0eda424b55f35dd256f0b48e0
Julian Meigen <julian.meigen@hotmail.de>
2024-05-17 19:00:54 +0200

    calclate strand ori percent per TFBS

--- E_Single_TFBS/Strand_Distance.py
+++ E_Single_TFBS/Strand_Distance.py
@@ -16,9 +16,22 @@ from scipy.stats import pearsonr, spearmanr
 path = r"H:\Uni - iCloud - Alte Semester\BA\All_TFBS.csv"
 df_combined = pd.read_csv(path)
 df_combined = df_combined[df_combined["chr"] != "chrM"]
-df_combined
+df_combined[df_combined["tf"]=="HMBOX1"]
 
 tfbs_multiple_df = df_combined[df_combined["homotypic_count"] > 1]
 tfbs_for_tf = tfbs_multiple_df[tfbs_multiple_df["tf"] == "HMBOX1"]
 tfbs_for_tf
+tfbs_multiple_df
+# Calculate Strand-Percent
 
+
+# change orientaion to 1 for nT and -1 for T strand in DataFrame
+df_change = tfbs_multiple_df.copy()
+df_change["strand_orientation"] = np.array([1 if i == "nT" else -1 for i in tfbs_multiple_df["strand_orientation"]])
+df_change
+
+# Calculate pct for orientation! in same len like df_change etc!
+test = df_change.groupby(by=["tf", "geneID", "homotypic_count"])["strand_orientation"].transform("mean")
+pct, count = np.unique(test, return_counts=True)
+for i in range(len(pct)):
+    print((pct[i], count[i]))
